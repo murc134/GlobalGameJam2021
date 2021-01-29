76153b2928fb3027cc6406277e9dbc7e17f6dd94
Christoph Brucksch <c.brucksch@googlemail.com>
2021-01-30 00:07:10 +0100

    Add possibility to assign multiple tags on GameObjects

---
+++ Assets/Scripts/Editor/ProjectChangedObserver.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+class ProjectChangedObserver
+{
+    static ProjectChangedObserver()
+    {
+        EditorSceneManager.sceneLoaded += OnSceneLoaded;
+        EditorApplication.hierarchyChanged += OnHierarchyChanged;
+    }
+
+    private static void OnHierarchyChanged()
+    {
+        ApplyTags();
+    }
+
+    private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ApplyTags();
+    }
+
+    [UnityEditor.Callbacks.DidReloadScripts]
+    private static void OnScriptsReloaded()
+    {
+        ApplyTags();
+    }
+
+    private static void ApplyTags()
+    {
+        GameObject[] gameObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        for(int i = 0; i<gameObjects.Length;i++)
+        {
+            TaggedObject tobj = gameObjects[i].GetComponent<TaggedObject>();
+
+            if(tobj == null)
+            {
+                gameObjects[i].AddComponent<TaggedObject>();
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/TagManager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "C4rtwork/Tag Manager")]
+public class TagManager : ScriptableObject
+{
+    private static TagManager instance = null;
+
+    public static TagManager Instance { 
+        get
+        {
+            if (instance == null) { instance = Resources.Load<TagManager>("TagManager"); }
+            return instance;
+        } 
+    }
+
+    [SerializeField]
+    private List<string> tags = new List<string>();
+    [SerializeField]
+    private List<string> layers = new List<string>();
+
+    public static List<string> Tags
+    {
+        get
+        {
+            return Instance.tags;
+        }
+    }
+
+    public static List<string> Layers
+    {
+        get
+        {
+            return Instance.layers;
+        }
+    }
+
+    public static List<TaggedObject> FindObjectsWithTag(string tag)
+    {
+        List<TaggedObject> list = new List<TaggedObject>();
+        TaggedObject[] array = GameObject.FindObjectsOfType<TaggedObject>();
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (!list.Contains(array[i]) && (array[i].Tags.Contains(tag) || array[i].tag == tag))
+            {
+                list.Add(array[i]);
+            }
+        }
+        return list;
+    }
+
+    public static TaggedObject FindObjectWithTag(string tag)
+    {
+        TaggedObject[] array = GameObject.FindObjectsOfType<TaggedObject>();
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Tags.Contains(tag) || array[i].tag == tag)
+            {
+                return array[i];
+            }
+        }
+        return null;
+    }
+
+    public static List<T> FindObjectsWithTag<T>(string tag)
+    {
+        List<T> list = new List<T>();
+        TaggedObject[] array = GameObject.FindObjectsOfType<TaggedObject>();
+        for (int i = 0; i < array.Length; i++)
+        {
+            T obj = array[i].gameObject.GetComponent<T>();
+
+            if (obj != null)
+            {
+                if (!list.Contains(obj) && (array[i].Tags.Contains(tag) || array[i].tag == tag))
+                {
+                    list.Add(obj);
+                }
+            }
+        }
+        return list;
+    }
+
+    public static T FindObjectWithTag<T>(string tag)
+    {
+        TaggedObject[] array = GameObject.FindObjectsOfType<TaggedObject>();
+        if (array.Length <= 0)
+        {
+            throw new IndexOutOfRangeException("There is no TaggedObjects in the scene");
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            T obj = array[i].gameObject.GetComponent<T>();
+
+            if (obj != null && (array[i].Tags.Contains(tag) || array[i].tag == tag))
+            {
+                return obj;
+            }
+        }
+        throw new KeyNotFoundException("There is no TaggedObjects in the scene with the tag " + tag);
+
+    }
+
+#if UNITY_EDITOR
+    public void Refresh()
+    {
+        layers = new List<string>(UnityEditorInternal.InternalEditorUtility.layers);
+        tags = new List<string>(UnityEditorInternal.InternalEditorUtility.tags);
+        tags.Remove("Untagged");
+    }
+#endif
+}
+
