8b070b3251a4e2c0e69b8ccc0df59ff40e448052
Christoph Brucksch <c.brucksch@googlemail.com>
2021-01-30 14:00:49 +0100

    PlayerController using NavMeshAgent

--- Assets/Scripts/MouseInteraction/MouseInputReceiver.cs
+++ Assets/Scripts/MouseInteraction/MouseInputReceiver.cs
@@ -7,43 +7,63 @@ using UnityEngine;
 
 public class MouseInputReceiver : ClickEventHandler
 {
+    public bool DebugInput = false;
     public override void HandleDrag(PointerInfo drag, PointerInfo drop)
     {
-        Debug.Log($"Drag {drag.HitTransform.name} on {drop.HitTransform.name}", gameObject);
+        if (DebugInput)
+        {
+            Debug.Log($"Drag {drag.HitTransform.name} on {drop.HitTransform.name}", gameObject);
+        }
     }
 
     public override void HandleDrop(PointerInfo drag, PointerInfo drop)
     {
-        Debug.Log($"Drop {drag.HitTransform.name} on {drop.HitTransform.name}", gameObject);
+        if (DebugInput)
+        {
+            Debug.Log($"Drop {drag.HitTransform.name} on {drop.HitTransform.name}", gameObject);
+        }
     }
 
     public override void HandleMouseClick1(PointerInfo info)
     {
-        Debug.Log($"Click1 {info.HitTransform.name}", gameObject);
+        if (DebugInput)
+        {
+            Debug.Log($"Click1 {info.HitTransform.name}", gameObject);
+        }
     }
-
     public override void HandleMouseClick2(PointerInfo info)
     {
-        Debug.Log($"Click2 {info.HitTransform.name}", gameObject);
+        if (DebugInput)
+        {
+            Debug.Log($"Click2 {info.HitTransform.name}", gameObject);
+        }
     }
-
     public override void HandleMouseDown(PointerInfo info)
     {
-        Debug.Log($"Down {info.HitTransform.name}", gameObject);
+        if (DebugInput)
+        {
+            Debug.Log($"Down {info.HitTransform.name}", gameObject);
+        }
     }
-
     public override void HandleMouseHold(PointerInfo info)
     {
-        Debug.Log($"Hold {info.HitTransform.name}", gameObject);
+        if (DebugInput)
+        {
+            Debug.Log($"Hold {info.HitTransform.name}", gameObject);
+        }
     }
-
     public override void HandleMouseHover(PointerInfo info)
     {
-        Debug.Log($"Hover {info.HitTransform.name}", gameObject);
+        if (DebugInput)
+        {
+            Debug.Log($"Hover {info.HitTransform.name}", gameObject);
+        }
     }
-
     public override void HandleMouseUp(PointerInfo info)
     {
-        Debug.Log($"Up {info.HitTransform.name}", gameObject);
+        if (DebugInput)
+        {
+            Debug.Log($"Up {info.HitTransform.name}", gameObject);
+        }
     }
 }
