4354acb8fc3873f21a611c3a56c9b99cd18e218e
Binary <binary@gmx.de>
2021-01-31 12:04:58 +0100

    Start Scene Update

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void LoadLevel ()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
