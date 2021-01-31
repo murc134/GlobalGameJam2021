66c46c25b9afe86d6ce05e30b258f128127fa9df
Christoph Brucksch <c.brucksch@googlemail.com>
2021-01-31 12:46:13 +0100

    Rename Music files

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,16 +4,7 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioClip menuIntro, menuLoop, mainLoop, dangerLoop, kinderzimmerLoop;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
